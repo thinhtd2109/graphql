edb61ec886870be7303e8efba51c41653efa6bdb
Duc Thinh <thinh.tranduc@digipaysolution.com>
2023-07-24 15:50:50 +0700

    handle user

--- services/database.service.ts
+++ services/database.service.ts
@@ -1,3 +1,3 @@
 import { Sequelize } from 'sequelize';
-const sequelize = new Sequelize('postgres://username:password@localhost:5432/database');
+const sequelize = new Sequelize('postgres://thinhtd2109:lZYL7PBvD9fg@ep-old-snowflake-877762-pooler.ap-southeast-1.aws.neon.tech/example-test');
 export default sequelize;
